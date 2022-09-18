18c4a872d17aae7d2173f2832cb07c410162f013
hens <121116111@qq.com>
2022-09-19 00:17:03 +0800

    feat: add unplugin plugin

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>index</div>
+  <nav-header />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { presetAttributify, presetUno } from 'unocss'
 import PostcssPxToViewport from 'postcss-px-to-viewport-8-plugin'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +15,17 @@ export default defineConfig({
       inspector: true,
       presets: [presetAttributify(), presetUno()]
     }),
-    Pages({})
+    Pages(),
+    Components({
+      dts: true
+    }),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router'],
+      eslintrc: {
+        enabled: true
+      }
+    })
   ],
   css: {
     postcss: {
